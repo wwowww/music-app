7c09fd1367d85ddd49c38070f7f064a131d0daca
wwowww <105021733+wwowww@users.noreply.github.com>
2024-11-19 18:21:35 +0900

    feat. router 기본 설정

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,14 +2,24 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import adminRoutes from "./routes/admin.route.js";
+import songRoutes from "./routes/song.route.js";
+import albumRoutes from "./routes/album.route.js";
+import statRoutes from "./routes/stat.route.js";
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.get("/users", (req, res) => {
-  // ...
-})
+app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/admin", adminRoutes)
+app.use("/api/songs", songRoutes)
+app.use("/api/albums", albumRoutes)
+app.use("/api/stats", statRoutes)
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
