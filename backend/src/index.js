80282f8e28eed97a05fadfb6fb8d040d240b8bc2
wwowww <105021733+wwowww@users.noreply.github.com>
2024-11-19 21:20:28 +0900

    feat. admin, auth controller 추가

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,6 +15,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json()); // to parse req.body
+
 app.use("/api/users", userRoutes)
 app.use("/api/auth", authRoutes)
 app.use("/api/admin", adminRoutes)
