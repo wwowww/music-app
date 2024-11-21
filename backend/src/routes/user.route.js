45d9d390f2c8327627ad9d8dcb51a767f6e69096
wwowww <105021733+wwowww@users.noreply.github.com>
2024-11-21 14:53:34 +0900

    feat. user controller 추가

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { getAllUsers } from "../controller/user.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  req.auth.userId
-  res.send("User route with get method")
-})
+router.get("/", protectRoute, getAllUsers);
 
 export default router;
