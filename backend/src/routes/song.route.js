88b19c80eaaa3cef1db037b6be3f949191ee779b
wwowww <105021733+wwowww@users.noreply.github.com>
2024-11-21 14:47:11 +0900

    feat. song controller 추가

--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { getAllSongs, getFeatureSongs, getMadeForYouSongs, getTrendingSongs } from "../controller/song.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Song route with GET method")
-})
+router.get("/", protectRoute, requireAdmin, getAllSongs);
+router.get("/featured", getFeatureSongs);
+router.get("/made-for-you", getMadeForYouSongs);
+router.get("/trending", getTrendingSongs);
 
 export default router;
