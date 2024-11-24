043b0de6e0fec17e5307d958d964dc66d10037a5
wwowww <105021733+wwowww@users.noreply.github.com>
2024-11-24 15:49:30 +0900

    feat. AuthCallbackPage 처리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import AuthCallbackPage from "./pages/auth/AuthCallbackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />} />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
       </Routes>
     </>
