619293b265805478eb72c06d1520d372e4b8fed6
wwowww <105021733+wwowww@users.noreply.github.com>
2024-11-24 14:31:32 +0900

    fix. route 오류 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
-import { Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import AuthCallbackPage from "./pages/auth/AuthCallbackPage";
 
 function App() {
   return (
     <>
-      <Route>
+      <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
-      </Route>
+      </Routes>
     </>
   )
 }
