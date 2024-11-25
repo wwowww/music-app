0ee07d456e9c680d7acc13c9e03afbaee5fecc57
wwowww <105021733+wwowww@users.noreply.github.com>
2024-11-25 23:07:50 +0900

    feat. music store, type, leftBar 컴포넌트 album 영역 추가

--- frontend/src/layout/components/LeftSideBar.tsx
+++ frontend/src/layout/components/LeftSideBar.tsx
@@ -1,13 +1,20 @@
 import PlaylistSkeleton from "@/components/skeletons/PlaylistSkeleton"
 import { buttonVariants } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
+import { useMusicStore } from "@/stores/useMusicStore"
 import { SignedIn } from "@clerk/clerk-react"
 import { ScrollArea } from "@radix-ui/react-scroll-area"
 import { HomeIcon, Library, MessageCircle } from "lucide-react"
+import { useEffect } from "react"
 import { Link } from "react-router-dom"
 
 const LeftSideBar = () => {
-  const isLoading = false;
+  const { albums, fetchAlbums, isLoading } = useMusicStore();
+
+  useEffect(() => {
+    fetchAlbums();
+  }, [fetchAlbums]);
+
   return (
     <div className="h-full flex flex-col gap-2">
       <div className="rounded-lg bg-zinc-900 p-4">
@@ -46,7 +53,23 @@ const LeftSideBar = () => {
               {isLoading ? (
                 <PlaylistSkeleton />
               ) : (
-                "sound music"
+                albums.map((album) => (
+                  <Link 
+                    to={`/albums/${album._id}`}
+                    key={album._id}
+                    className="p-2 hover:bg-zinc-800 rounded-md flex items-center gap-3 group cursor-pointer"
+                  >
+                    <img 
+                      src={album.imageUrl}
+                      alt="PlayList image."
+                      className="size-12 rounded-md flex-shrink-0 object-cover"
+                    />
+                    <div className="flex-1 min-w-0 hidden md:block">
+                      <p className="font-medium truncate">{album.title}</p>
+                      <p className="text-sm text-zinc-400 truncate">Album • {album.artist}</p>
+                    </div>
+                  </Link>
+                ))
               )}
           </div>
         </ScrollArea>
