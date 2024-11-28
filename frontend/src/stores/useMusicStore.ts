82ef6d042709be43ec6e9f40a805fac4502c82f4
wwowww <105021733+wwowww@users.noreply.github.com>
2024-11-28 12:58:33 +0900

    feat. album page, 로그인 activity, music player 추가

--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -7,7 +7,9 @@ interface MusicStore {
   songs: Song[];
   isLoading: boolean;
   error: string | null;
+  currentAlbum: Album | null;
   fetchAlbums: () => void;
+  fetchAlbumById: (id: string) => Promise<void>;
 }
 
 export const useMusicStore = create<MusicStore>((set) => ({
@@ -15,17 +17,29 @@ export const useMusicStore = create<MusicStore>((set) => ({
   songs: [],
   isLoading: false,
   error: null,
+  currentAlbum: null,
 
   fetchAlbums: async () => {
-    set({isLoading: true, error: null});
+		set({ isLoading: true, error: null });
 
-    try {
-      const response = await axiosInstance.get("/albums");
-      set({albums: response.data})
-    } catch (error: any) {
-      set({error: error.response.data.message});
-    } finally {
-      set({isLoading: false})
-    }
-  }
+		try {
+			const response = await axiosInstance.get("/albums");
+			set({ albums: response.data });
+		} catch (error: any) {
+			set({ error: error.response.data.message });
+		} finally {
+			set({ isLoading: false });
+		}
+	},
+  fetchAlbumById: async (id) => {
+		set({ isLoading: true, error: null });
+		try {
+			const response = await axiosInstance.get(`/albums/${id}`);
+			set({ currentAlbum: response.data });
+		} catch (error: any) {
+			set({ error: error.response.data.message });
+		} finally {
+			set({ isLoading: false });
+		}
+	},
 }))
